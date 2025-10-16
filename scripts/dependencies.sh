bf7e5204af44139eed55873017bd9e3cec0e55af
scmeek <sean@seanmeek.com>
2025-10-16 10:41:47 -0600

    Add initial scripts and Makefile

---
+++ scripts/dependencies.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -euo pipefail
+
+SCRIPTS_DIR="${SCRIPTS_DIR:-$(dirname -- "$(readlink -f -- "$0")")}"
+PROJECT_ROOT="${PROJECT_ROOT:-$(CDPATH='' cd -- "$SCRIPTS_DIR/../.." && pwd)}"
+
+. "${SCRIPTS_DIR}/functions.sh"
+
+cargo install cargo-tarpaulin --locked
+cargo install cargo-workspace-lints
