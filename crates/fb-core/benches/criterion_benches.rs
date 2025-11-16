540ee866f76e4e705ec45154d314541489b48f40
scmeek <sean@seanmeek.com>
2025-11-15 17:48:46 -0700

    test: enable benchmark but have it empty for now

--- crates/fb-core/benches/criterion_benches.rs
+++ crates/fb-core/benches/criterion_benches.rs
@@ -1,12 +1,13 @@
 use criterion::{Criterion, criterion_group, criterion_main};
-use fb_core::add;
-use std::hint::black_box;
+// use fb_core::add;
+// use std::hint::black_box;
 
-fn bench_add(c: &mut Criterion) {
-    c.bench_function("my_function", |b| {
-        b.iter(|| add(black_box(42), black_box(58)));
-    });
-}
+fn empty(_c: &mut Criterion) {}
+// fn bench_add(c: &mut Criterion) {
+//     c.bench_function("my_function", |b| {
+//         b.iter(|| add(black_box(42), black_box(58)));
+//     });
+// }
 
-criterion_group!(benches, bench_add);
+criterion_group!(benches, empty);
 criterion_main!(benches);
