ef74ecf99dbc66917153a7479c5c4bdd1c3dc75d
scmeek <sean@seanmeek.com>
2025-11-16 11:36:23 -0700

    ci: Attempt fix for benchmark workflow

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -13,7 +13,7 @@ fi
 
 echo ""
 
-BENCHMARK_CMD="cargo criterion --bench criterion_benches" # Also in dependencies.sh
+BENCHMARK_CMD="cargo criterion" # Also in dependencies.sh
 info "Running benchmarks..."
 if ! $BENCHMARK_CMD; then
   fail "Benchmarks failed."
