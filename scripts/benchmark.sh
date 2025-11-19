a70f781315ff9e277dbb9bf1c19a43425e983728
scmeek <sean@seanmeek.com>
2025-11-18 23:01:12 -0700

    ci: attempt PR post fix and make benches dynamic based on "bench = true"

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -13,7 +13,7 @@ fi
 
 echo ""
 
-BENCHMARK_CMD="cargo bench --bench criterion_benches" # Also in dependencies.sh and fb-core Cargo.toml
+BENCHMARK_CMD="cargo bench --benches"
 info "Running benchmarks..."
 if ! $BENCHMARK_CMD; then
   fail "Benchmarks failed."
